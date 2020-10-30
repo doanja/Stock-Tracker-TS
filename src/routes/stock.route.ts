adc00646a2131e4f414ab3cd3f8585292775e8dc
doanja <doanja@plu.edu>
2020-10-30 14:54:44 -0700

    created routes for stock

---
+++ src/routes/stock.route.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+// import { addFavorite, getFavorites, removeFavorite } from '../controllers/recipe.controller';
+import { verifyAccessToken } from '../middleware/verifyToken';
+
+export default class RecipeRoute {
+  public router: Router = Router();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  public initializeRoutes() {
+    // this.router.get('/favorites', [verifyAccessToken], getFavorites);
+    // this.router.put('/favorites/add', [verifyAccessToken], addFavorite);
+    // this.router.put('/favorites/remove', [verifyAccessToken], removeFavorite);
+    this.router.get('/stock/hour/:sampleSize'); // show 24 to represent 1 day
+    this.router.get('/stock/day/:sampleSize'); // show 7 to represent 1 week
+    this.router.get('/stock/week/:sampleSize'); // show 4 to represent 1 month
+    this.router.get('/stock/month/:sampleSize'); // show 12 to represent 1 year
+  }
+}
