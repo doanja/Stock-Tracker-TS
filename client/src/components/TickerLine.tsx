08cd4b04db09ae9550570bcd252c64c81e7641ac
doanja <doanja@plu.edu>
2020-11-13 16:11:47 -0800

    updated ticker styles and layout

--- client/src/components/TickerLine.tsx
+++ client/src/components/TickerLine.tsx
@@ -4,39 +4,48 @@ import { Link } from 'react-router-dom';
 import '../styles/ticker.min.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faCaretSquareUp, faCaretSquareDown } from '@fortawesome/free-solid-svg-icons';
 
 interface TickerLineProps {
   tickerPrices?: TickerPrice[];
 }
 
 const TickerLine: React.FC<TickerLineProps> = ({ tickerPrices }) => {
-  // each ticker box, needs the name, current price, how much it went up/down
-
-  <FontAwesomeIcon icon={faArrowUp} />;
-
   return (
     <Container className='mt-3'>
       <Row>
         {tickerPrices?.map(ticker => (
           <Col md='2' className='ticker-item'>
-            <Link to={`/quote/${ticker.symbol}`} key={ticker.symbol}>
-              <Row>
-                <Col md={4}>{ticker.prices[1].priceChange < 0 ? <FontAwesomeIcon icon={faArrowDown} /> : <FontAwesomeIcon icon={faArrowUp} />}</Col>
-                <Col md={4}>
-                  <h3 className='text-center'>{ticker.symbol}</h3>
-                  <h3 className='text-center'>{ticker.prices[1].price}</h3>
-                </Col>
-                <Col md={4}>
-                  {ticker.prices[1].priceChange < 0 ? (
-                    <h3 className='text-center'>-${ticker.prices[1].priceChange * -1}</h3>
-                  ) : (
-                    <h3 className='text-center'>${ticker.prices[1].priceChange}</h3>
-                  )}
-
-                  <h3 className='text-center'>{ticker.prices[1].changePercent}%</h3>
-                </Col>
-              </Row>
+            <Link to={`/quote/${ticker.symbol}`} style={{ textDecoration: 'none' }} key={ticker.symbol}>
+              {ticker.prices[1].priceChange < 0 ? (
+                <Row>
+                  <Col xs={2}>
+                    <FontAwesomeIcon className='ticker-icon ticker-icon-red' icon={faCaretSquareDown} />
+                  </Col>
+                  <Col xs={5}>
+                    <p className='font-weight-bold'>{ticker.symbol}</p>
+                    <p>{ticker.prices[1].price}</p>
+                  </Col>
+                  <Col xs={5} className='text-danger'>
+                    <p>${ticker.prices[1].priceChange * -1}</p>
+                    <p>{ticker.prices[1].changePercent}%</p>
+                  </Col>
+                </Row>
+              ) : (
+                <Row>
+                  <Col xs={2}>
+                    <FontAwesomeIcon className='ticker-icon ticker-icon-green' icon={faCaretSquareUp} />
+                  </Col>
+                  <Col xs={5}>
+                    <p className='font-weight-bold'>{ticker.symbol}</p>
+                    <p>{ticker.prices[1].price}</p>
+                  </Col>
+                  <Col xs={5} className='text-success'>
+                    <p>${ticker.prices[1].priceChange}</p>
+                    <p>+{ticker.prices[1].changePercent}%</p>
+                  </Col>
+                </Row>
+              )}
             </Link>
           </Col>
         ))}
