30dd8b6d483fe2275e789a8d319bed0927b8417b
doanja <doanja@plu.edu>
2020-12-04 12:29:21 -0800

    finished styling footer component

--- client/src/components/CustomFooter.tsx
+++ client/src/components/CustomFooter.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Navbar, NavbarBrand, Container } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import '../styles/footer.min.css';
+import '../styles/main.min.css';
 
 const CustomFooter: React.FC = () => {
   return (
-    <div className='fixed-bottom custom-footer'>
-      <Navbar color='dark'>
-        <Container>
-          <NavbarBrand>Footer</NavbarBrand>
-        </Container>
-      </Navbar>
-    </div>
+    <Navbar bg='dark' variant='dark' className='fixed-bottom custom-footer'>
+      <Container>
+        <div className='text-center'>
+          <Navbar.Brand className='brand' href='/'>
+            Stock Tracker
+          </Navbar.Brand>
+          <Nav className='mr-auto'>
+            <Nav.Link href='/help'>Help</Nav.Link>
+            <Nav.Link href='/privacy'>Privacy</Nav.Link>
+            <Nav.Link href='/terms'>Terms</Nav.Link>
+          </Nav>
+        </div>
+      </Container>
+    </Navbar>
   );
 };
 
