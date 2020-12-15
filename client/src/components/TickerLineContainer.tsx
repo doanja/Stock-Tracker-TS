14351e2ff3d023967a9e7dab74e3f297b0ccebc4
doanja <doanja@plu.edu>
2020-12-14 22:07:22 -0800

    removed unused depedencies in tickerlinecontainer

--- client/src/components/TickerLineContainer.tsx
+++ client/src/components/TickerLineContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { TickerLine } from './';
 import { Container, Row } from 'react-bootstrap';
 import '../styles/ticker.min.css';
