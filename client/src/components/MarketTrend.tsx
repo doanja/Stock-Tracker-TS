4f2e9a836bc4fb110bf034c0799f7fd5922ca3ae
doanja <doanja@plu.edu>
2020-12-16 15:42:19 -0800

    initial import variables into MarketTrend component

--- client/src/components/MarketTrend.tsx
+++ client/src/components/MarketTrend.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import '../styles/main.min.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+
+// redux
+import { useDispatch, useSelector } from 'react-redux';
+import { RootStore } from '../redux/Store';
+import { setTicker, addToWatchlist, removeFromWatchlist } from '../redux/actions/stockActions';
 
 interface MarketTrendProps {
   tickerPrice: TickerPrice;
 }
 
 const MarketTrend: React.FC<MarketTrendProps> = ({ tickerPrice }) => {
-  return <p>{tickerPrice.companyName}</p>;
+  const history = useHistory();
+  const [isWatching, setIsWatching] = useState(false);
+
+  // redux
+  const { watchlist } = useSelector((state: RootStore) => state.stock);
+  const { loginStatus } = useSelector((state: RootStore) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (loginStatus) watchlist.includes(tickerPrice.symbol) ? setIsWatching(true) : setIsWatching(false);
+  }, [tickerPrice, watchlist]);
+
+  const saveTicker = (tickerSymbol: string) => {
+    if (loginStatus) {
+      isWatching ? dispatch(removeFromWatchlist(tickerSymbol)) : dispatch(addToWatchlist(tickerSymbol));
+    } else history.push('/login');
+  };
+
+  return (
+    <div className='market-card' onClick={() => dispatch(setTicker(tickerPrice.symbol))}>
+      <div className='market-badge'>{tickerPrice.symbol}</div>
+      <p className='market-name'>{tickerPrice.companyName}</p>
+      <p className='market-price'>{tickerPrice.prices[0].price}</p>
+      {isWatching ? (
+        <FontAwesomeIcon className='discover-icon icon' icon={faMinusCircle} size='lg' onClick={() => saveTicker(tickerPrice.symbol)} />
+      ) : (
+        <FontAwesomeIcon className='discover-icon icon' icon={faPlusCircle} size='lg' onClick={() => saveTicker(tickerPrice.symbol)} />
+      )}
+    </div>
+  );
 };
 
 export default MarketTrend;
