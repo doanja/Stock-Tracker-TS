7c2bca3e27fd09f21862d0b7de5ca93b738bbbf4
doanja <doanja@plu.edu>
2021-01-06 13:12:17 -0800

    updated StockService routes

--- client/src/services/StockService.tsx
+++ client/src/services/StockService.tsx
@@ -9,16 +9,28 @@ export default class StockService {
     return axios.get<any>('/stock/pricesMin');
   }
 
-  public getWatchlist(): Promise<AxiosResponse<any>> {
+  public getWatchlists(): Promise<AxiosResponse<any>> {
     return axios.get<any>('/watchlist');
   }
 
-  public addToWatchlist(ticker: string): Promise<AxiosResponse<any>> {
-    return axios.put<any>('/watchlist/add', { ticker });
+  public createWatchlist(): Promise<AxiosResponse<any>> {
+    return axios.post<any>('/watchlist');
   }
 
-  public removeFromWatchlist(ticker: string): Promise<AxiosResponse<any>> {
-    return axios.put<any>('/watchlist/remove', { ticker });
+  public updateWatchlistName(watchlistId: string, name: string): Promise<AxiosResponse<any>> {
+    return axios.put<any>(`/watchlist/add/${watchlistId}/${name}`);
+  }
+
+  public addToWatchlist(watchlistId: string, ticker: string): Promise<AxiosResponse<any>> {
+    return axios.put<any>(`/watchlist/add/${watchlistId}/${ticker}`);
+  }
+
+  public removeFromWatchlist(watchlistId: string, ticker: string): Promise<AxiosResponse<any>> {
+    return axios.put<any>(`/watchlist/remove/${watchlistId}/${ticker}`);
+  }
+
+  public deleteWatchlist(watchlistId: string): Promise<AxiosResponse<any>> {
+    return axios.delete<any>(`/watchlist/${watchlistId}`);
   }
 
   public getCompanyInfo(companyName: string): Promise<any> {
